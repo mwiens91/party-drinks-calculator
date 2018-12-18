b6633e80efb336889acd9d5b77fe33b37a027c5d
Matt Wiens <mwiens91@gmail.com>
2018-12-18 15:59:44 -0800

    Add missing bit of e433d780ceea924bc67ee85d9c4e9e733b07039d

--- party_drinks_calculator/version.py
+++ party_drinks_calculator/version.py
@@ -1,5 +1,5 @@
 """Contains name, version, description."""
 
-NAME = "party-drinks-calculator"
+NAME = "party drinks calculator"
 VERSION = "1.0.0"
 DESCRIPTION = "calculate required party drinks inventory"
