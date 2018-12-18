e030ef7a5f0e5cfbc7b03e6defe9a56a8d8f7130
Matt Wiens <mwiens91@gmail.com>
2018-12-17 17:22:42 -0800

    Add constants we need

---
+++ party_drinks_calculator/constants.py
@@ -0,0 +1,11 @@
+"""Useful constants for the program."""
+
+# How many drinks per quantity of liquor type
+DRINKS_PER_BEER = 6             # 6 x 330mL
+DRINKS_PER_WINE = 5             # 750mL
+DRINKS_PER_HARD_LIQUOR = 16     # 750mL
+
+# Verbose name for our liquor quantities
+BEER_VERBOSE_NAME = "6 x 330mL beer"
+WINE_VERBOSE_NAME = "750mL bottle wine"
+HARD_LIQUOR_VERBOSE_NAME = "750mL bottle hard liquor"
